19bfdf40e02041d66373960f2f5c69b95f686f80
frehman <fike.rehman@hotmail.com>
2023-04-19 19:48:21 -0500

    Updates

---
+++ precache-manifest.43add8134c0c62f6593c6be17c53cd7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d33595804fdc272ac3e",
+    "url": "/foxtrotRomeo/static/css/main.2d62fd88.chunk.css"
+  },
+  {
+    "revision": "2d33595804fdc272ac3e",
+    "url": "/foxtrotRomeo/static/js/main.2d335958.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/foxtrotRomeo/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/foxtrotRomeo/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "c56666124c327cc30308",
+    "url": "/foxtrotRomeo/static/js/runtime~main.c5666612.js"
+  },
+  {
+    "revision": "ca5dd3ad45e537f01faa2cc7218d271d",
+    "url": "/foxtrotRomeo/static/media/mylogo.ca5dd3ad.svg"
+  },
+  {
+    "revision": "fbfb07a961ec371530ea308de1f3c788",
+    "url": "/foxtrotRomeo/index.html"
+  }
+];
